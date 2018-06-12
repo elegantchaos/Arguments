24b9705bc094292dfd3f936b6a734aac99cb8fbe
Sam Deane <sam@elegantchaos.com>
2018-06-12 14:18:43 +0100

    Added support for altering the argument values, and for setting up without using Docopt.
    Useful for testing clients of this package, where we want to be able to
    spoof certain inputs to them.

--- Sources/Arguments/Arguments.swift
+++ Sources/Arguments/Arguments.swift
@@ -5,10 +5,10 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 /**
- Argument parsing.
- Uses Docopt for the heavy lifting, but performs a bit of preliminary
- cleanup first, and provides a simplified API to read options and arguments.
- */
+Argument parsing.
+Uses Docopt for the heavy lifting, but performs a bit of preliminary
+cleanup first, and provides a simplified API to read options and arguments.
+*/
 
 import Docopt
 import Logger
@@ -16,18 +16,19 @@ import Logger
 public struct Arguments {
 
     public enum Failure : Error {
-        case unknownOption(name : String)
+    case unknownOption(name : String)
     }
 
     let program : String
-    private let parsed : [String:Any]
+    private var parsed : [String:Any]
+
 
     /**
-     Parse the command line arguments.
+    Parse the command line arguments.
 
-     Any recognised options are pulled out into the options dictionary.
-     Other arguments end up in the unused array (so that they can be passed on to a sub-process, for example).
-     */
+    Any recognised options are pulled out into the options dictionary.
+    Other arguments end up in the unused array (so that they can be passed on to a sub-process, for example).
+    */
 
     public init(documentation : String) {
         let filteredArguments = Manager.removeLoggingOptions(from: CommandLine.arguments)
@@ -35,11 +36,12 @@ public struct Arguments {
         self.parsed = Docopt.parse(documentation, argv: Array(filteredArguments[1...]), help: true, version: "1.0")
     }
 
+
     /**
-     Return an option.
-     It's an error to try to read an option that wasn't passed in when
-     we were set up.
-     */
+    Return an option.
+    It's an error to try to read an option that wasn't passed in when
+    we were set up.
+    */
 
     public func option(_ name : String) throws -> String  {
         if let value = parsed["--\(name)"] as? String {
@@ -50,8 +52,8 @@ public struct Arguments {
     }
 
     /**
-     Return an option, or a default value if it's missing.
-     */
+    Return an option, or a default value if it's missing.
+    */
 
     public func option(_ name : String, `default`: String) -> String  {
         if let value = parsed["--\(name)"] as? String {
@@ -61,8 +63,19 @@ public struct Arguments {
     }
 
     /**
-     Return an argument, or a default value if it's missing.
-     */
+    Check a boolean option.
+    */
+
+    public func option(_ name : String) -> Bool  {
+        if let value = parsed["--\(name)"] as? Bool {
+            return value
+        }
+        return false
+    }
+
+    /**
+    Return an argument, or a default value if it's missing.
+    */
 
     public func argument(_ name : String, `default`: String = "") -> String  {
         if let value = parsed["<\(name)>"] as? String {
@@ -72,13 +85,64 @@ public struct Arguments {
     }
 
     /**
-     Was a particular command given?
-     */
+    Was a particular command given?
+    */
 
     public func command(_ name: String) -> Bool {
         if let value = parsed[name] as? Bool {
             return value
         }
         return false
+    }
+
+    /// MARK: Testing support.
+
+    /**
+    Init with an explicit program name and set of parsed arguments.
+
+    This is intended for testing, where it's useful to be able to mock
+    the arguments.
+    */
+
+    public init(program: String, parsed : [String:Any] = [:]) {
+        self.program = program
+        self.parsed = parsed
+    }
+
+    /**
+    Set an option.
+    Generally just used for testing, but can also be used to modify options
+    during the course of execution.
+    */
+
+    public mutating func setOption(_ name: String, value: Any) {
+        parsed["--\(name)"] = value
+    }
+
+    /**
+    Set an argument.
+    Generally just useful for testing.
+    */
+
+    public mutating func setArgument(_ name: String, value: Any) {
+        parsed["<\(name)>"] = value
+    }
+
+    /**
+    Set a command.
+    Generally just useful for testing.
+    */
+
+    public mutating func setCommand(_ name: String) {
+        parsed[name] = true
+    }
+
+    /**
+    Clear a command.
+    Generally just useful for testing.
+    */
+
+    public mutating func clearCommand(_ name: String) {
+        parsed[name] = false
     }
 }
