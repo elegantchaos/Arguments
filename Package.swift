3f054899c97edba76749e17bc32517ed04912ac7
Sam Deane <sam@elegantchaos.com>
2018-12-11 17:57:11 +0000

    updated to Docopt 0.6.9

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.0.8"),
-        .package(url: "https://github.com/elegantchaos/docopt.swift", from: "0.6.8"),
+        .package(url: "https://github.com/elegantchaos/docopt.swift", from: "0.6.9"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
