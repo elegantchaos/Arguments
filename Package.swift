18fbecce70cdad8677aac45d73d80af6eee4e839
Sam Deane <sam@elegantchaos.com>
2019-06-17 12:13:09 +0100

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["Arguments"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.2"),
-        .package(url: "https://github.com/elegantchaos/docopt.swift", from: "0.6.10"),
+        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.3.5"),
+        .package(url: "https://github.com/elegantchaos/docopt.swift.git", from: "0.6.10"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
