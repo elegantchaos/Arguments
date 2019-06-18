5806917ccc9fa252b2de5e4dcbfde755fbff90e3
Sam Deane <sam@elegantchaos.com>
2019-06-18 17:34:50 +0100

    updated packages, require 10.13

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Arguments",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,8 +15,8 @@ let package = Package(
             targets: ["Arguments"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.3.5"),
-        .package(url: "https://github.com/elegantchaos/docopt.swift.git", from: "0.6.10"),
+        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.3.6"),
+        .package(url: "https://github.com/elegantchaos/docopt.swift.git", from: "0.6.11"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
